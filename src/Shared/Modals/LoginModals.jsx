86d534f225ea010c6836900c820734a57567c772
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-10-09 15:31:44 +0600

    modal designed

--- src/Shared/Modals/LoginModals.jsx
+++ src/Shared/Modals/LoginModals.jsx
@@ -2,18 +2,24 @@
 
 import GithubSignin from "../SignInButton/GithubSignin";
 import GoogleSignin from "../SignInButton/GoogleSignin";
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
 const LoginModals = ({ showModal, handleClose }) => {
   if (!showModal) return null;
 
+  //todo close by esc button and click other place in screen
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white p-6 rounded-md w-[400px] relative">
+      <div className="bg-white pb-7 px-9 pt-12 rounded-xl w-[400px] relative">
+        <p className="font-semibold text-slate-600 text-center text-xl mb-7">
+          Log in to Create and Save Anime
+        </p>
         <button
-          className="absolute top-2 right-2 text-red-500"
+          className="absolute top-1 right-1 text-red-500 text-4xl"
           onClick={handleClose}
         >
-          Close
+          <IoMdCloseCircleOutline></IoMdCloseCircleOutline>{" "}
         </button>
 
         <div className="my-2">
