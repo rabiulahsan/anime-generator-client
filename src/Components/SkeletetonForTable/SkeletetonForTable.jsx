c59ca690360f6eef1f5aba2f89bb5210961ff8a3
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-10-15 15:42:11 +0600

    create skeleton for table

---
+++ src/Components/SkeletetonForTable/SkeletetonForTable.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+// for skeleton style
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const SkeletetonForTable = ({ number }) => {
+  const skeletonElements = [];
+  for (let i = 0; i < number; i++) {
+    skeletonElements.push(
+      <div key={i}>
+        <div className="">
+          <Skeleton
+            count={2}
+            style={{ marginBottom: "12px" }}
+            baseColor="#2c2c2c" // Dark base color
+            highlightColor="#444" // Slightly lighter to emphasize the flowing effect
+            duration={0.5} // Adjusts speed of the flow effect
+          ></Skeleton>
+        </div>
+      </div>
+    );
+  }
+
+  return <>{skeletonElements}</>;
+};
+
+export default SkeletetonForTable;
--- src/pages/Payments/Payments.jsx
+++ src/pages/Payments/Payments.jsx
@@ -1,11 +1,31 @@
+import { Player } from "@lottiefiles/react-lottie-player";
 import SectionTitle from "../../Components/SectionTitle/SectionTitle";
 import Navbar from "../Home/Navbar/Navbar";
+import UseMyAllPayments from "../../Hooks/UseMyAllPayments/UseMyAllPayments";
+import SkeletetonForTable from "../../Components/SkeletetonForTable/SkeletetonForTable";
 
 const Payments = () => {
+  const [myPayments, isLoading] = UseMyAllPayments();
   return (
     <div>
       <Navbar></Navbar>
-      <SectionTitle heading="All of your Purchase"></SectionTitle>
+      {myPayments.length === 0 ? (
+        // Display 404 if no anime cards
+        <div className="flex justify-center items-center my-[3%]">
+          <div className="">
+            <Player className="h-[300px]" autoplay loop src="/no.json"></Player>
+            <p className="text-3xl font-extrabold text-slate-600 text-center mt-[5%]">
+              You don&apos;t have any purchasae
+            </p>
+          </div>
+        </div>
+      ) : (
+        <>
+          <SectionTitle heading="All of your Purchase"></SectionTitle>
+          {/* Show skeletons while loading */}
+          {isLoading && <SkeletetonForTable number={10}></SkeletetonForTable>}
+        </>
+      )}
     </div>
   );
 };
